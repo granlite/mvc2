aa426823b8b460d7cca76d277af0dfc3666d8016
granlite <65130690+granlite@users.noreply.github.com>
2020-09-15 21:47:30 +0400

    Add files via upload

---
+++ mvc2/HomeController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quadratic.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            ViewBag.Message = "description";
+            return View();
+        }
+        public ActionResult Quadratic(double a, double b, double c)
+        {
+            var car = new { Manufacturer = "bmw", Model = "a320", MaxSpeed = 300 };
+            ViewBag.carinfo = car.MaxSpeed;
+            double d = Math.Pow(b, 2) - 4 * a * c;
+            double e = Math.Pow(d, 0.5);
+            double x1 = (-b - e) / (2 * a);
+            double x2 = (-b + e) / (2 * a);
+            ViewBag.x1 = x1;
+            ViewBag.x2 = x2;
+            if (d < 0)
+            {
+                return View("Quad0");
+            }
+            else if (d == 0)
+            {
+                return View("Quad1");
+            }
+            else
+            {
+                return View("Quad2");
+            }
+        }
+    }
+}
